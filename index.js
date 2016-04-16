c378a33792ac049ec720145d6d39856792569361
BRANDON PARKER <Brandon@dkparker.com>
2016-04-16 16:05:42 -0700

    updated the gulpfile and the errors that it was throwing in order to run gulp by itself

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // require Router and instantiate
 var Router = require(__dirname + '/lib/router.js');
-router = new Router();
+var router = new Router();
 
 // require server and launch
-exports.server = require('http').createServer(router.route()).listen(8888, () => console.log('Server spinning on Port: 8888'));
+exports.server = require('http').createServer(router.route()).listen(8888, () => {
+  console.log('Server spinning on Port: 8888');
+});
 
 // require and export all major js files
 exports.Router = require(__dirname + '/lib/router.js');
