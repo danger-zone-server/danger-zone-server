c6ce33c761d1585195b7ba424d9825f0780689cd
BRANDON PARKER <Brandon@dkparker.com>
2016-04-16 16:22:04 -0700

    Deleted unnecessary files in lib and test

--- test/writeFile_test.js
+++
@@ -1,5 +0,0 @@
-// require chai, chai-http, fs, writeFile.js
-  it('Should create a new text file named something unique, like sequential or time-stamped')
-  it('Should not overwrite a file with the same name as it.')
-  it('Should return an error string when a parameter besides callback is not defined.')
-  it('Should return an error string when a parameter is not an appropriate type.')
