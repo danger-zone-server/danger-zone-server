27be052030be65e6f6e0bc57f813a915546079c0
James Norton <james.norton@me.com>
2016-04-16 14:54:58 -0700

    integrate headMessage to server and integrate server into index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Router = require(__dirname + '/lib/router.js');
 router = new Router();
 
 // require server and launch
-exports.server = require('http').createServer(router.route()).listen(3000);
+exports.server = require('http').createServer(router.route()).listen(8888, () => console.log('Server spinning on Port: 8888'));
 
 // require and export all major js files
 exports.Router = require(__dirname + '/lib/router.js');
