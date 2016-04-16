238bdc480414b6a35433a73022e6735d75abdc04
KMSkelton <kmskelton@outlook.com>
2016-04-16 14:53:26 -0700

    setting up tests

--- test/router_test.js
+++ test/router_test.js
@@ -30,7 +30,10 @@ describe('data store ', () => {
 //  require chai, chai-http, fs at least
 // possible tests
 describe('The router ', () => {
-  it('Should create a router object with routes when the constructor method is called.')
+  it('Should create a router object with routes when the constructor method is called.', (done) => {
+    request
+    
+  })
   it('Should define a callback function for an http GET (POST, PUT, PATCH, DELETE) request and url that returns an expected value.'){
   it('Should 404 when a server using the Router object is requested a url which does not exist.')
 })
