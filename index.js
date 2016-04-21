0fa1b3a46e73eb3d42181a3ca0430bf2b8f2e9aa
James Norton <james.norton@me.com>
2016-04-21 11:11:04 -0700

    update README to correct errors and improve clarification, move require on server out of function and to the top of the index.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Require server and launch
+const httpServer = require('http');
+
 var PORT = 3000;
 if (process.argv[2]) {
   PORT = process.argv[2];
 }
 
+// Require server and launch
 exports.server = function(routerCall) {
-    require('http').createServer(routerCall).listen(PORT, () => {
+    httpServer.createServer(routerCall).listen(PORT, () => {
     console.log('Server requesting flyby on Port: ' + PORT);
   });
 };
 
 // Require and export all major js files
-exports.Router = require(__dirname + '/lib/router.js');
-exports.headMessage = require(__dirname + '/lib/headMessage.js');
+exports.Router = require(__dirname + '/lib/router');
+exports.headMessage = require(__dirname + '/lib/headMessage');
